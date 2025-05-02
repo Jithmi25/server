c4a220c88e813c319cfdd4520744d4284ef8382b
Jithmi <wickjitha@gmail.com>
2025-05-02 21:41:57 +0530

    install libraries and index.js

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+  const express = require('express');
+  const dotenv = require('dotenv');
+  const cors = require('cors');
+  const morgan = require('morgan');
+  const path = require('path');
+  
+  dotenv.config();
+  
+  const app = express();
+  
+  
+  app.use(cors());
+  app.use(express.json());
+  app.use(morgan('dev'));
+
+  
+  app.use((req, res) => {
+    res.status(404).json({ message: 'Route not found' });
+  });
+  
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => {
+    console.log(`🚀 Server running on http://localhost:${PORT}`);
+  });
   
